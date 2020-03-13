b7d80c21e6457f2aeb4d62507fa1a1985873f5cd
Kyle Imhoff <imhoff.kyle@gmail.com>
2020-03-13 20:55:12 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -22,4 +22,11 @@ class Transfer
       return "Transaction rejected. Please check your account balance."
     end
   end
+  def reverse_transfer
+    if @status == "complete"
+      @sender.balance += @amount
+      @receiver.balance -= @amount
+      @status = "reversed"
+    end
+  end
 end
