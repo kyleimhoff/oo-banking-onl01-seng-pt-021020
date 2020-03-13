d457306df457164297d97e166a484a93443b3d52
Kyle Imhoff <imhoff.kyle@gmail.com>
2020-03-13 20:40:24 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,3 +1,9 @@
 class Transfer
   attr_accessor :sender, :reciver, :amount, :status
+  
+  def initialize(sender, reciver, amount)
+    @sender = sender 
+    @reciver = reciver 
+    @amount = amount
+    
 end
