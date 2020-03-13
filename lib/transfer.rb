6dce9f192803c4a0d53d9754a25318015aa17428
Kyle Imhoff <imhoff.kyle@gmail.com>
2020-03-13 21:00:07 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -13,7 +13,7 @@ class Transfer
   end
   
   def execute_transaction
-    if @sender.balance > @amount && @status == "pending"
+    if valid? && sender.balance > amount && self.status == "pending"
       @sender.balance -= @amount
       @receiver.balance += @amount
       @status = "complete"
